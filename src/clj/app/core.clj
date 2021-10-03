8451907858a5110d39fc2b0b35c6bd5d3ba9e103
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-10-03 16:17:07 -0400

    require verifier

--- src/clj/app/core.clj
+++ src/clj/app/core.clj
@@ -4,7 +4,9 @@
    [taoensso.timbre :as timbre
     :refer [log  trace  debug  info  warn  error  fatal  report
             logf tracef debugf infof warnf errorf fatalf reportf
-            spy get-env]]))
+            spy get-env]]
+   
+   [app.verifier]))
 
 (defn -main []
   (debugf "MEM %dG" (/ (-> (Runtime/getRuntime) .maxMemory) 1024 1024))
