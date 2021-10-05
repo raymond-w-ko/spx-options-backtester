8f6ba973976e2d474d4b69f4c9b60c983e1cdd67
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-10-04 21:23:06 -0400

    rm old build.clj

--- build/build.clj
+++ build/build.clj
@@ -1,12 +1,5 @@
 (ns build
-  (:require [badigeon.javac :as j]))
-
-(defn javac []
-  (println "Compiling Java")
-  (j/javac "src" {:compile-path     "classes"
-                  :compiler-options ["-cp" "src:classes" "-target" "1.10"
-                                     "-source" "1.10" "-Xlint:-options"]})
-  (println "Compilation Completed"))
+  (:require ))
 
 (defn -main []
   (javac))
